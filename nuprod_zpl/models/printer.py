ca34a96a3d97ee95cb718b06acdebf6329c00439
Nuprod <106673064+nuprod@users.noreply.github.com>
2022-08-09 13:19:03 +0200

    modified:   nuprod_zpl/models/printer.py

--- nuprod_zpl/models/printer.py
+++ nuprod_zpl/models/printer.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api, _
 import logging
 import socket
-from nuprod_zpl.module.zplNuprod import *
+import zpl
 from odoo.exceptions import UserError
 
 class nuprod_zpl(models.Model):
