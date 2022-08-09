23fae5984ced6e7737c16334088cac5cb6164c34
Nuprod <106673064+nuprod@users.noreply.github.com>
2022-08-09 10:52:57 +0200

    new file:   nuprod_zpl/__init__.py 	new file:   nuprod_zpl/__manifest__.py 	new file:   nuprod_zpl/controllers/__init__.py 	new file:   nuprod_zpl/controllers/controllers.py 	new file:   nuprod_zpl/demo/demo.xml 	new file:   nuprod_zpl/models/__init__.py 	new file:   nuprod_zpl/models/printer.py 	new file:   nuprod_zpl/models/report.py 	new file:   nuprod_zpl/security/ir.model.access.csv 	new file:   nuprod_zpl/static/description/icon.png 	new file:   nuprod_zpl/views/report_socket_view.xml 	new file:   nuprod_zpl/views/stock_product_view.xml 	new file:   nuprod_zpl/views/templates.xml

---
+++ nuprod_zpl/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import printer
+from . import report
---
+++ nuprod_zpl/models/printer.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+import logging
+import socket
+import zpl
+from odoo.exceptions import UserError
+
+class nuprod_zpl(models.Model):
+
+    _description = 'nuprod_zpl'
+    _name = "epl.printer"
+
+    name = fields.Char('name', size=256, required=True)
+    ip_address = fields.Char('Ip Address', size=32, required=True)
+    port = fields.Integer('Port', required=True)
+    
+    logger = logging.getLogger(__name__)
+    logger.warning('Nuprod ZPL Load')
+
+    def connection(self,printer_id):
+        printer = self.browse(printer_id)
+
+        ip = printer.ip_address
+        port = printer.port
+        # buffer_size = printer.buffer_size
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        res = {
+            'socket': s,
+            'ip': ip,
+            'port': port,
+        }
+        return res
+
+    def print_report(self, id, data):
+        """Print epl report
+        :param data: unicode string to print
+        """
+        printer = self.browse(id)
+        if not printer:
+            raise Warning(_("Printer not found"))
+        port = printer.port
+        ip = printer.ip_address
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip, port))
+            s.send(data)
+            s.close()
+        except Exception as e:
+            print("Couldn't send epl printing: %s" % e)
+            # logger.error('Failed to do something: ' + str(e))
+
+class nuprod_print_product(models.Model):
+
+    _inherit = "product.template"
+
+    def print_label_nuprod(self):
+        logger = logging.getLogger(__name__)
+        for record in self:
+            if record.barcode:
+                dpmm = 8
+                l = zpl.Label(51,30,dpmm)
+                height = 0
+                char_size = 6
+                barcode_width = 3.5
+                l.origin(25,4,'0')
+                l.write_text(record.default_code, char_height=char_size, char_width=char_size, justification='C')
+                l.endorigin()
+                height += 10  
+                #l.origin((len(record.barcode)*barcode_width)/2,height)
+                l.origin(27,height)
+                l.write_barcode(height=60, barcode_type='Q',magnification=6)
+                l.write_text(record.barcode,qrcode=True)
+                l.endorigin()
+                connection_printer = self.env["epl.printer"].connection(1)
+                self.env["epl.printer"].print_report(1,(bytes(l.dumpZPL(), "utf8")))
+            else:
+                raise UserError(
+                            _(
+                                "Pas de code barre sur le produit " + str(record.default_code) + "\n" +
+                                "Veuillez en définir un pour imprimer l'étiquette"
+                            )
+                        )
