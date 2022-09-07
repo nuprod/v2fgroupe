f3ad12153c15aa99393a5264705e0769e97ec8f8
Nuprod <106673064+nuprod@users.noreply.github.com>
2022-09-07 16:07:28 +0200

    modified:   nuprod_zpl/models/printer.py

--- nuprod_zpl/models/printer.py
+++ nuprod_zpl/models/printer.py
@@ -3,7 +3,7 @@
 from odoo import models, fields, api, _
 import logging
 import socket
-import zpl
+from . import zplNuprod
 from odoo.exceptions import UserError
 
 class nuprod_zpl(models.Model):
@@ -18,7 +18,6 @@ class nuprod_zpl(models.Model):
         comodel_name="res.company",
         inverse_name="name",
         string="Localisation in company")
-
     logger = logging.getLogger(__name__)
     logger.warning('Nuprod ZPL Load')
 
@@ -68,7 +67,7 @@ class nuprod_print_product(models.Model):
         for record in self:
             if record.barcode:
                 dpmm = 8
-                label = zpl.Label(56, 30, dpmm)
+                label = zplNuprod.Label(56, 30, dpmm)
                 height = 0
                 char_size = 6
                 margeGauche = 10
